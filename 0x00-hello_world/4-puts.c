7411f83320cd3c53475bf302a9ba41ab3c3d0d96
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-13 14:31:32 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,23 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 
 /**
  *
- *  *
+ *  *  * main - Entry point
  *
- *   *  * main - The code entry point
+ *   *   *
  *
- *    *
+ *    *    * Return: Always 0 (Success)
  *
- *     *   *
+ *     *     *
  *
- *      *
- *
- *       *    * Return: Always 0 (Success)
- *
- *        *
- *
- *         *     */
-
-
+ *      */
 
 int main(void)
-	
+
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+			puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
-
+						return (0);
 
 }
