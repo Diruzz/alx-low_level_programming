14605fba48e450f906204d643cc1d8e29d10d3ee
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-15 12:50:09 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 
 {
 	int i, c;
-	i= 0;
+	i = 0;
+	
 	while (i < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
-		i++;
-		
+		i++;		
 		_putchar('\n');
 	}
 
