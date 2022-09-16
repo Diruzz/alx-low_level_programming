5fd2d1c7c5c9cfb5f0a77973a9376d7501d365eb
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-16 01:38:25 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,7 +3,7 @@
 	* main - main block
 	* Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
 	* Numbers must be coma and space separated.
-	* Return 0.
+	* Return: 0
 	*/
 int main(void)
 {
