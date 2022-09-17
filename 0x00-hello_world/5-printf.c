34171eac2655d269fd521b368b73bf9d48da420d
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-17 12:53:42 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,35 +1,10 @@
-//#!/bin/bash
 #include <stdio.h>
 /**
- *
- *  *
- *
- *   *  * main - The code entry point
- *
- *    *
- *
- *     *   *
- *
- *      *
- *
- *       *    * Return: Always 0 (Success)
- *
- *        *
- *
- *         *     */
-
-
-
+	* main - The code entry point
+	* Return: Always 0 (Success)
+	*/
 int main(void)
-
-
-
 {
-
-			printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-						return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
