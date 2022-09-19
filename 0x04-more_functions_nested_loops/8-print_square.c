375cca57e94a4238277548a6d0d9b12621752215
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-19 03:16:51 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+	* print_square - prints a square
+	* @size: print size
+	* Return: Always 0
+	*/
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar ('\n');
+		}
+	}
+}
