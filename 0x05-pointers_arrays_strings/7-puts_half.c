6a0a4496111da7a9a053023098713455cd2365a4
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-20 14:37:50 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,6 @@
 void puts_half(char *str)
 {
 	int j = 0;
-	char _putchar();
 	int k;
 
 	while (str[j] != '\0')
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,9 @@
 int _putchar(char c);
 
 
+char _putchar(void);
+
+
 void reset_to_98(int *n);
 
 
