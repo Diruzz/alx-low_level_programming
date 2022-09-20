46797762d6e08c162d49a2b49ad50a45cf1e8f8e
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-19 22:13:32 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
-/**_puts - prints a string
+/**
+	* _puts - prints a string
 	* @str: string to print
 	* Description: prints a string
 	* On success: returns no error
