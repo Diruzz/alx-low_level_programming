45727683cd4f9983231c80674e8638f6205f2aa4
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-21 13:28:49 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,8 +9,8 @@ void reverse_array(int *a, int n)
 	int tmp, index;
 
 	for (index = n - 1; index > n / 2; index--)
-		tmp = a[n - 1 - index];
 	{
+		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
