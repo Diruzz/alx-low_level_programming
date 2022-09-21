c529f771420c2273d0fb3de7a451bafa3b5395c7
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-21 12:50:12 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,13 +6,16 @@
  *@n:integer parameter to compare index to
  *Return: returns new concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 	
 	while (dest[index++])
 		dest_len++;
+
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
