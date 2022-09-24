462dc554269c5b79772b11f60dc4e7b7909f80a1
Arnold Mukilima <diruarnold082@gmail.com>
2022-09-23 19:55:10 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
